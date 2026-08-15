df3c7ac3844e4f569b52c3497d7ee3c20953f377
MahboobGeeky <mahboob9184@gmail.com>
2026-08-15 20:39:36 +0530

    completed

--- 07_RESTfull_API/db/book.js
+++ 07_RESTfull_API/db/book.js
@@ -1,4 +1,4 @@
-// in memory database (Models)
+// in memory database
 exports.BOOKS = [
   { id: 1, title: "Book One", author: "Author One" },
   { id: 2, title: "Book Two", author: "Author Two" },
