40d888ac6db3a6c05b23956ee91060141a6ae222
MahboobGeeky <mahboob9184@gmail.com>
2026-08-19 23:58:31 +0530

    Node-ORM

---
+++ 09_Postgres/db/index.js
@@ -0,0 +1,6 @@
+const {drizzle} = require('drizzle-orm/node-postgres');
+
+// postgres://<username>:<password>@<host>:<port>/<db_name>
+const db = drizzle("postgres://postgres:admin@localhost:5432/mydb");
+
+module.exports = db;
