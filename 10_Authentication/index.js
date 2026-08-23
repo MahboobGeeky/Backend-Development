a1363613fcad66d5072058b61de53e241ef7d324
MahboobGeeky <mahboob9184@gmail.com>
2026-08-24 00:22:10 +0530

    Authentication setup

---
+++ 10_Authentication/index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+
+const app = express()
+const PORT = 8000
+
+const DIARY = {};
+const EMAILS = new Set();
+
+// Hey, Here is my car -> please park it and give me back a token
+// Email => Unique Car Number
+
+app.post('/signup', (req, res) => {
+    const { name, email, password } = req.body;
+    if(EMAILS.has(email)) {
+        return res.status(400).json({error: 'Email already taken'});
+    }
+
+    // create a token for user
+    const token = `${Date.now()}`;
+
+    // Do a entry in diary
+    DIARY[token] = {name, email, password};
+    EMAILS.add(email);
+
+    return res.json({status: 'success', token});
+});
+
+app.post('/me', (req, res) => {
+    const {token} = req.body;
+    if(!token){
+        return res.status(404).json({ Error: `Missing Token`});
+    }
+
+    if( !(token in DIARY)) {
+        return res.status(404).json({ Error: `Invalid Token`});
+    }
+
+});
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
