7b9be6dea92a64988123a5d9bf999625ce9a7b9e
MahboobGeeky <mahboob9184@gmail.com>
2026-09-12 09:29:34 +0530

    done

--- 13_Docker/index.js
+++ 13_Docker/index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 
-const PORT = 8000;
+const PORT = process.env.PORT ? +process.env.PORT : 8000;
 
 const app = express();
 
 app.get("/", (req, res) => {
-  return res.json({ status: "success", message: "Hello from server..." });
+  return res.json({
+    status: "success",
+    message: "Hello from server...",
+  });
+});
+
+app.get("/health", (req, res) => {
+  return res.status(200).json({ message: "I am healthy" });
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
 });
+
